ea01b6d32a2ee8b5f50ff95ed6f3a30da3469a7f
bbgjr <ganjiaren9@gmail.com>
2025-09-23 15:00:56 +0800

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -5,7 +5,7 @@ EMAIL="g175773823@gmail.com"   # 登录邮箱
 PASSWORD="Gjr@650801"     # 登录密码
 
 # 要保活的URL列表,多个用英文空格分隔
-URLS="https://xxxx.cfapps.ap21.hana.ondemand.com https://xxxx.cfapps.us10-001.hana.ondemand.com"  
+URLS="https://augeakir.cfapps.ap21.hana.ondemand.com https://aubbsduj.cfapps.ap21.hana.ondemand.com"  
 
 # 颜色定义
 green() {
